3c7756e02871e8a8658b1598a967383d5bb9d60d
bptiago <bptiago123@gmail.com>
2024-09-09 13:36:50 -0300

    Add responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,15 @@ import ColoredTabs from "./ColoredTabs";
 
 export default function Navbar() {
   return (
-    <header className="w-100 p-10">
-      <div className="flex justify-between items-center mx-auto">
-        <div className="flex justify-between items-center gap-2">
-          <FingerprintIcon fontSize="large" color="primary" />
-          <span className="font-semibold text-2xl">tiago</span>
-        </div>
+    <header className="sticky top-0 w-100 py-4 px-20 bg-white border-b">
+      <div className="flex justify-center sm:justify-between items-center mx-auto">
+        <a href="/">
+          <div className="hidden sm:flex justify-between items-center gap-2">
+            <FingerprintIcon fontSize="large" color="primary" />
+            <span className="font-semibold text-2xl">tiago</span>
+          </div>
+        </a>
+
         <div>
           <ColoredTabs labels={["home", "contato", "projetos"]} />
         </div>
