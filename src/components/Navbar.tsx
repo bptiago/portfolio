4f2ee8545d810d5bfcf0cf1ea09b97f1b0678d7c
bptiago <bptiago123@gmail.com>
2024-09-12 16:15:20 -0300

    Fix navbar href

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 w-100 py-4 px-20 bg-white border-b">
       <div className="flex justify-center sm:justify-between items-center mx-auto">
-        <a href="/">
+        <a href="#home">
           <div className="hidden sm:flex justify-between items-center gap-2">
             <FingerprintIcon fontSize="large" color="primary" />
             <span className="font-semibold text-2xl">tiago</span>
