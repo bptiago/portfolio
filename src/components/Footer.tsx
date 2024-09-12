ec4bc19098e6148f27557d995ab8196f53e98dac
bptiago <bptiago123@gmail.com>
2024-09-12 14:14:40 -0300

    UI adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,15 +5,15 @@ export default function Footer() {
   return (
     <div className="border-t p-6 flex justify-center gap-x-8">
       <a href="https://github.com/bptiago" target="_blank">
-        <div className="flex items-center gap-x-1 underline underline-offset-2 hover:no-underline">
-          <GitHubIcon fontSize="medium" />
+        <div className="flex items-center gap-x-1 underline underline-offset-2 hover:no-underline text-sm">
+          <GitHubIcon fontSize="small" />
           GitHub
         </div>
       </a>
 
       <a href="https://www.linkedin.com/in/bptiago/" target="_blank">
-        <div className="flex items-center gap-x-1 underline underline-offset-2 hover:no-underline">
-          <LinkedInIcon fontSize="medium" />
+        <div className="flex items-center gap-x-1 underline underline-offset-2 hover:no-underline text-sm">
+          <LinkedInIcon fontSize="small" />
           LinkedIn
         </div>
       </a>
--- src/components/RepositoryCard.tsx
+++ src/components/RepositoryCard.tsx
@@ -1,4 +1,3 @@
-import { Link } from "@mui/material";
 import { ComponentPropsWithoutRef } from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
@@ -10,31 +9,30 @@ type CardProps = {
 
 export default function RepositoryCard({ title, description, url }: CardProps) {
   return (
-    <div
-      className="w-full min-w-54 rounded hover:shadow-[0_0px_20px_0px_rgba(25,118,210,0.25)] border hover:cursor-pointer"
-      style={{ transition: "all", transitionDuration: "0.3s" }}
-    >
+    <a href={url} target="_blank">
       <div
-        className="h-52 flex items-center justify-center p-4"
-        style={{
-          background: "#F9F8FD",
-        }}
+        className="w-full min-w-54 rounded hover:shadow-[0_0px_20px_0px_rgba(25,118,210,0.25)] border hover:cursor-pointer"
+        style={{ transition: "all", transitionDuration: "0.3s" }}
       >
-        <div className="text-center text-sm tertiary-color">
-          {description ? description : <GitHubIcon />}
+        <div
+          className="h-52 flex items-center justify-center p-4"
+          style={{
+            background: "#F9F8FD",
+          }}
+        >
+          <div className="text-center text-sm tertiary-color">
+            {description ? description : <GitHubIcon />}
+          </div>
+        </div>
+        <div className="p-6 border-t" style={{ background: "#ffffff" }}>
+          <h1 className="highlight-color w-fit font-semibold text-md">
+            {title}
+          </h1>
+          <div className="hover:tracking-wide hover:underline transition-all">
+            github.com
+          </div>
         </div>
       </div>
-      <div className="p-6 border-t" style={{ background: "#ffffff" }}>
-        <h1 className="highlight-color w-fit font-semibold text-md">{title}</h1>
-        <Link
-          href={url}
-          underline="hover"
-          sx={{ color: "black" }}
-          className="hover:tracking-wide transition-all"
-        >
-          github.com
-        </Link>
-      </div>
-    </div>
+    </a>
   );
 }
